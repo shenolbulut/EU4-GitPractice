6b601aa5a06a88a45341a1a3ed009f2d3464be25
ilyastunch <iltun85@gmail.com>
2021-02-12 06:36:38 +0300

    more changes movie2

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -4,6 +4,7 @@ public class HelloTest {
         System.out.println("EU4 is super");
         System.out.println("movie2 is created");
         //changes for movie2
+        //more changes movie2
 
     }
 }
