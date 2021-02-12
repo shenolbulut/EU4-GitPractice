e3d0bf74390fe91b5dd5840d23abceffe2424bb9
ilyastunch <iltun85@gmail.com>
2021-02-12 06:10:46 +0300

    added changes FeatureBTest

--- src/main/java/FeatureBTest.java
+++ src/main/java/FeatureBTest.java
@@ -1,5 +1,7 @@
 public class FeatureBTest {
     public static void main(String[] args) {
         System.out.println("More works for featureB");
+        System.out.println("More work feature B test" +
+                "");
     }
 }
