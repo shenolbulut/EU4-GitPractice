ec63e7860e5bb0d51d76960ba9d5492b9c698fbf
ilyastunch <iltun85@gmail.com>
2021-02-12 06:32:12 +0300

    movie1 updated Hello Java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, EU4");
         //Watching more movie in bedroom
+        //ready to go back
     }
 
 }
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello world Test");
         System.out.println("EU4 is super");
+
     }
 }
