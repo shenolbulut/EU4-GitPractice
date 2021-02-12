3eaa703a47d8cc0104860daec4ad4b5a42c216fe
ilyastunch <iltun85@gmail.com>
2021-02-12 06:30:33 +0300

    master updated Hello Java

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,6 @@
 public class HelloTest {
     public static void main(String[] args) {
-        System.out.println("hello world Test");
+        System.out.println("hello world Test EU5");
         System.out.println("EU4 is super");
     }
 }
