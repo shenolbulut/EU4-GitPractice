3ffb4ce6f75ca53d5f75e5af2f4aea9f05c7df4b
ilyastunch <iltun85@gmail.com>
2021-02-12 06:37:44 +0300

    more changes master

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello world Test EU5");
         System.out.println("EU4 is super");
+        //changes in maste
 
     }
 }
